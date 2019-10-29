a621b8a5b82b487b07ff57fd7e6ef2c6692bf852
baldm0mma <jev.forsberg@gmail.com>
2019-10-29 16:44:19 -0600

    Add top 5 projects to content array plus image assets

--- src/content/content.js
+++ src/content/content.js
@@ -4,6 +4,7 @@ export const content = [
     title: 'My Eighties Computer (NPM Package)',
     description: "This is the first NPM Package I've ever written! I love contributing to free, opensource code, and I'm really excited for this to be my first home-baked contribution! There are four components in total, a styled h1, a styled paragraph text, and two styled and controlled forms. The h1 and paragraph texts take a single prop, which is the text that they will display; and the two form components take two props each; one is the text title for the form, and the second is a callback function that returns the current controlled state of the form inputs. It was a blast to make, and I hope someone gets some use out of it!",
     gitHub: 'https://github.com/baldm0mma/my-eighties-computer',
+    gitLab: '',
     live: 'https://baldm0mma.github.io/my-eighties-computer/',
     image: 'src/assets/My80sComputer.png'
   },
@@ -12,38 +13,43 @@ export const content = [
     title: 'Home Safe',
     description: "Rome wasn't built in a day; but this app was! A finalist for the 2019 Tech to Protect Hackathon, Home Safe gives homeowners an easy way to upload their pertinent safety information to be stored and displayed in a useful manner. It encourages users to complete a safety checklist, upload floor plans, and create a pre-incident plan to be printed and saved for display in the home. All of these features culminate to help homeowners get insurance breaks and offer first responders an amazing set of data that will help them do their job.",
     gitHub: 'https://github.com/baldm0mma/home-safe',
+    gitLab: '',
     live: 'https://ttp-home-safe.herokuapp.com/',
     image: 'src/assets/HomeSafe.png'
   },
   {
     id: 03,
-    title: '',
-    description: '',
-    gitHub: '',
+    title: 'Ride with Me',
+    description: "Ride With Me is a mobile application written in React Native that serves as a community board for motorcyclists to connect with other riders. Upon creation of an account, users can log in, add friends, and join rides based on criteria that they so choose, such as location, distance, and type of ride. Check out our front end code on GitHub, and our back end GraphQL database on GitLab.",
+    gitHub: 'https://github.com/baldm0mma/ride_with_me',
+    gitLab: 'https://gitlab.com/ryanmillergm/motorcycle_ride',
     live: '',
-    image: ''
+    image: 'src/assets/RideWithMe.gif'
   },
   {
     id: 04,
-    title: '',
-    description: '',
-    gitHub: '',
-    live: '',
-    image: ''
+    title: 'Richard Mortimer',
+    description: "This project is Rick and Morty fan app. With the announcement of Rick and Morty Season 4 coming this winter, there's a lot of excitement around it's hotly anticipated release. This app is designed to be a endorsement piece; an attempt to keep up the excitement for the upcoming season. There's a bit of gamification, a bit of shameless plugging of Adult Swim, and a LOT of 80's computer styling.",
+    gitHub: 'https://github.com/baldm0mma/richard_mortimer',
+    gitLab: '',
+    live: 'https://rich-mort.herokuapp.com/',
+    image: 'src/assets/RichardMortimer.gif'
   },
   {
     id: 05,
-    title: '',
-    description: '',
-    gitHub: '',
-    live: '',
-    image: ''
+    title: 'Kouleur-FE',
+    description: "Kouleur-FE is the React front-end of a full-stack color palette picker application. We used Semantic UI elements to style and design an elegant, thoughtful, and intuitive UI/UX experience. This front end app pairs with a PostgreSQL/Node.js/Express.js RESTful API which we also built our ourselves that utilizes semantic HTTP methods, intuitive url pathway naming conventions, and a one-to-many database of our own design. You can find a link to the back end on our front end GitHub page.",
+    gitHub: 'https://github.com/baldm0mma/kouleur-fe',
+    gitLab: '',
+    live: 'https://kouleur.herokuapp.com/',
+    image: 'src/assets/Kouleur.gif'
   },
   {
     id: 06,
     title: '',
     description: '',
     gitHub: '',
+    gitLab: '',
     live: '',
     image: ''
   },
@@ -52,6 +58,7 @@ export const content = [
     title: '',
     description: '',
     gitHub: '',
+    gitLab: '',
     live: '',
     image: ''
   },
@@ -60,6 +67,7 @@ export const content = [
     title: '',
     description: '',
     gitHub: '',
+    gitLab: '',
     live: '',
     image: ''
   }
