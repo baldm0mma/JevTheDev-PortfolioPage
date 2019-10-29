6b7002efec269a616373987eb47e496ef7ebcce0
baldm0mma <jev.forsberg@gmail.com>
2019-10-29 17:15:55 -0600

    Add first portfolio piece and transitions

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { NameBadge } from './NameBadge';
+import { Content } from './Content';
+import { projectContent } from '../projectContent/projectContent';
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
 import './App.css';
 
 export const App = () => {
   return (
     <main>
       <NameBadge />
+      <Zoom>
+        <h4 className='portfolio-title'>Portfolio</h4>
+        <img className='down-arrow' src='./assets/downArrow.png' />
+      </Zoom>
+      <Fade left>
+        <Content content={projectContent[0]} />
+      </Fade>
     </main>
   );
 };
