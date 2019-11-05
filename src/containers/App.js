5ca4dd619407d7ba506a3293ed6dff103ae8cce9
baldm0mma <jev.forsberg@gmail.com>
2019-11-05 13:22:21 -0700

    Remove NameBadge

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { About } from './About';
 import { Content } from './Content';
-// import { NameBadge } from './NameBadge';
 import { projectContent, aboutContent } from '../projectContent/projectContent';
 import { wakeHerokuApps } from '../fetchCalls';
 import Fade from 'react-reveal/Fade';
@@ -15,7 +14,12 @@ export const App = () => {
 
   return (
     <main>
-      {/* <NameBadge /> */}
+      <Zoom>
+        <article className='nameBadge-container'>
+          <h2>Welcome to _jevTheDev</h2>
+          <p>Hi, I'm Jev; This is my portfolio.</p>
+        </article>
+      </Zoom>
       <Zoom>
         <h4 className='about-title'>About</h4>
       </Zoom>
