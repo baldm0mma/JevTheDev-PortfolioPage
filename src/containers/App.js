4a2d795729cca7cc5e6f123b71d05da626390be6
baldm0mma <jev.forsberg@gmail.com>
2019-11-05 12:59:40 -0700

    Change Recat import to top

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import { About } from './About.js';
 import { Content } from './Content.js';
@@ -5,7 +6,6 @@ import { NameBadge } from './NameBadge.js';
 import { projectContent, aboutContent } from '../projectContent/projectContent';
 import { wakeHerokuApps } from '../fetchCalls.js';
 import Fade from 'react-reveal/Fade';
-import React, { useEffect } from 'react';
 import Zoom from 'react-reveal/Zoom';
 
 export const App = () => {
