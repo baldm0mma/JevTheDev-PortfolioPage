ab5db75313d8cc7d8f388c22958991814d3c2e89
baldm0mma <jev.forsberg@gmail.com>
2019-11-05 13:18:18 -0700

    Test if namebadge is breaking everything

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { About } from './About';
 import { Content } from './Content';
-import { NameBadge } from './NameBadge';
+// import { NameBadge } from './NameBadge';
 import { projectContent, aboutContent } from '../projectContent/projectContent';
 import { wakeHerokuApps } from '../fetchCalls';
 import Fade from 'react-reveal/Fade';
