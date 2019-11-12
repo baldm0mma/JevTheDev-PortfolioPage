c2389b0af8166fbe8150ca2a13ce267e7665e952
baldm0mma <jev.forsberg@gmail.com>
2019-11-12 09:31:04 -0700

    Change default state of test reducer

--- src/reducers/testReducer.js
+++ src/reducers/testReducer.js
@@ -1,8 +1,8 @@
-export const testReducer = (state = 'test', action) => {
+export const testReducer = (state = 'test', { type, changedState }) => {
   const { type } = action;
   switch (type) {
     case 'TEST_STORE':
-      return state;
+      return changedState;
     default:
       return state;
   }
