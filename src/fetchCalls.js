23c3c863d87a0376db48ec09734a7b3206cd1b4a
baldm0mma <jev.forsberg@gmail.com>
2019-12-10 17:25:29 -0700

    Reinstate kouleur api fetch call

--- src/fetchCalls.js
+++ src/fetchCalls.js
@@ -1,5 +1,5 @@
 const urls = [
-  // 'https://cors-anywhere.herokuapp.com/https://kouleur-api.herokuapp.com/',
+  'https://cors-anywhere.herokuapp.com/https://kouleur-api.herokuapp.com/',
   'https://cors-anywhere.herokuapp.com/https://kouleur.herokuapp.com/',
   'https://cors-anywhere.herokuapp.com/https://nasa-apod-final.herokuapp.com/',
   'https://cors-anywhere.herokuapp.com/https://rich-mort.herokuapp.com/',
