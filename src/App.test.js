1cb0291f884ba578eb77aa12ade7e9c9c74949c4
baldm0mma <jev.forsberg@gmail.com>
2019-12-12 13:33:51 -0700

    Add snapshot test to App

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,26 @@ import React from "react";
 import { shallow } from "enzyme";
 import { App } from "./App";
 
-it("should render without crashing", () => {
-  const component = shallow(<App />);
-  expect(component).toBeTruthy();
-});
+describe("App component", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  it("should render without crashing", () => {
+    expect(wrapper).toBeTruthy();
+  });
 
-it("renders a welcome message", () => {
-  const wrapper = shallow(<App />);
+  it("should match the snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-  // Approach #1 - assert that shallow wrapper will contain specific React element (exactly as it appears in the code).
-  const welcome = <h1 data-testId="page-title">Welcome to _jevTheDev</h1>;
-  expect(wrapper).toContainReact(welcome);
+  it("renders a welcome message", () => {
+    // Approach #1 - assert that shallow wrapper will contain specific React element (exactly as it appears in the code).
+    const welcome = <h1 data-testId="page-title">Welcome to _jevTheDev</h1>;
+    expect(wrapper).toContainReact(welcome);
 
-  // Approach #2 - assert that a search of shallow wrapper by specific data attribute will render a specific text.
-  const welcomeMessage = wrapper.find("[data-testId='page-title']").text();
-  expect(welcomeMessage).toBe("Welcome to _jevTheDev");
+    // Approach #2 - assert that a search of shallow wrapper by specific data attribute will render a specific text.
+    const welcomeMessage = wrapper.find("[data-testId='page-title']").text();
+    expect(welcomeMessage).toBe("Welcome to _jevTheDev");
+  });
 });
