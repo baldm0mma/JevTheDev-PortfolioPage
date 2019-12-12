2c4cf29e5bdb6844d950323096745b8ceb19510b
baldm0mma <jev.forsberg@gmail.com>
2019-12-12 12:49:29 -0700

    Add note about running full test coverage report in create-react-app

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+// Test with full coverage report
+// npm test -- --coverage --watchAll=false
