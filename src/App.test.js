25f6be8e35e86f5cc937d9019341ce7da38d88cb
baldm0mma <jev.forsberg@gmail.com>
2019-12-13 12:17:27 -0700

    Finish testing @ 88.64 |    88.89 |    93.75 |     87.8 %

--- src/App.test.js
+++ src/App.test.js
@@ -15,16 +15,6 @@ describe("App component", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("renders a welcome message", () => {
-    // Approach #1 - assert that shallow wrapper will contain specific React element (exactly as it appears in the code).
-    const welcome = <h1 data-testid="page-title">Welcome to _jevTheDev</h1>;
-    expect(wrapper).toContainReact(welcome);
-
-    // Approach #2 - assert that a search of shallow wrapper by specific data attribute will render a specific text.
-    const welcomeMessage = wrapper.find("[data-testid='page-title']").text();
-    expect(welcomeMessage).toBe("Welcome to _jevTheDev");
-  });
-
   // it("should call wakeHerokuApps() on mount", () => {
   //   const wakeHerokuApps = jest.fn();
   //   expect(wakeHerokuApps).toHaveBeenCalled();
--- src/containers/NameBadge/NameBadge.test.js
+++ src/containers/NameBadge/NameBadge.test.js
@@ -2,4 +2,29 @@ import React from "react";
 import { shallow } from "enzyme";
 import { NameBadge } from "./NameBadge";
 
-describe
+describe("NameBadge Component", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NameBadge />);
+  });
+
+  it("should render without crashing", () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it("should match the snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render a welcome message", () => {
+    // Approach #1 - assert that shallow wrapper will contain specific React element (exactly as it appears in the code).
+    const welcome = <h1 data-testid="page-title">Welcome to _jevTheDev</h1>;
+    expect(wrapper).toContainReact(welcome);
+
+    // Approach #2 - assert that a search of shallow wrapper by specific data attribute will render a specific text.
+    const welcomeTitle = wrapper.find("[data-testid='page-title']").text();
+    expect(welcomeTitle).toBe("Welcome to _jevTheDev");
+    const welcomeText = wrapper.find("[data-testid='page-text']").text();
+    expect(welcomeText).toBe("Hi, I'm Jev. I'm a Dev. This is my portfolio.");
+  });
+});
