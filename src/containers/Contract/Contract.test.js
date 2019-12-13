9c76c603fb6236dde8a3b9cbaee5390cac2d83fd
baldm0mma <jev.forsberg@gmail.com>
2019-12-13 11:42:47 -0700

    Add Contract test file and describe block

--- src/containers/Contract/Contract.test.js
+++ src/containers/Contract/Contract.test.js
@@ -1,4 +1,11 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { Contract } from "./Contract";
+import { contractContent } from "../../allData/allData.js";
 
+describe("Contract Component", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Contract content={content} />);
+  });
+});
