e3dc502e5f1a6af8ebce203cbd3df58130b3f50f
jev forsberg <jev.forsberg@grafana.com>
2025-08-01 11:24:55 -0600

    import icon css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import { Contact } from "./containers/Contact/Contact";
 import { Contract } from "./containers/Contract/Contract";
 import { FullTime } from "./containers/FullTime/FullTime";
 import { NameBadge } from "./containers/NameBadge/NameBadge";
-import {
-  // projectContent,
-  contractContent,
-  fullTimeContent,
-} from "./allData/allData.js";
+import { contractContent, fullTimeContent } from "./allData/allData.js";
 import { wakeHerokuApps } from "./apiCalls";
 import Fade from "react-reveal/Fade";
 import Zoom from "react-reveal/Zoom";
@@ -68,57 +64,6 @@ export const App = () => {
       <Fade left>
         <Contract content={contractContent[1]} />
       </Fade>
-      {/* <Zoom>
-        <h4 className="about-title">Personal Projects</h4>
-      </Zoom>
-      <img
-        className="down-arrow"
-        src="./assets/downArrow.png"
-        alt="Down Arrow"
-      />
-      <Fade right>
-        <Content content={projectContent[0]} />
-      </Fade>
-      <img
-        className="down-arrow"
-        src="./assets/downArrow.png"
-        alt="Down Arrow"
-      />
-      <Fade left>
-        <Content content={projectContent[1]} />
-      </Fade>
-      <img
-        className="down-arrow"
-        src="./assets/downArrow.png"
-        alt="Down Arrow"
-      />
-      <Fade right>
-        <Content content={projectContent[4]} />
-      </Fade>
-      <img
-        className="down-arrow"
-        src="./assets/downArrow.png"
-        alt="Down Arrow"
-      />
-      <Fade left>
-        <Content content={projectContent[2]} />
-      </Fade>
-      <img
-        className="down-arrow"
-        src="./assets/downArrow.png"
-        alt="Down Arrow"
-      />
-      <Fade right>
-        <Content content={projectContent[3]} />
-      </Fade>
-      <img
-        className="down-arrow"
-        src="./assets/downArrow.png"
-        alt="Down Arrow"
-      />
-      <Fade left>
-        <Content content={projectContent[5]} />
-      </Fade> */}
       <Fade top>
         <Contact />
       </Fade>
